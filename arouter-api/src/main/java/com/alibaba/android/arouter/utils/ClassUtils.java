14a5add66efc1d62384a2b301fd791e20281b319
正纬 <zhilong.lzl@alibaba-inc.com>
2017-05-08 14:51:29 +0800

    Release dex file after route map process.

--- arouter-api/src/main/java/com/alibaba/android/arouter/utils/ClassUtils.java
+++ arouter-api/src/main/java/com/alibaba/android/arouter/utils/ClassUtils.java
@@ -56,18 +56,30 @@ public class ClassUtils {
     public static List<String> getFileNameByPackageName(Context context, String packageName) throws PackageManager.NameNotFoundException, IOException {
         List<String> classNames = new ArrayList<>();
         for (String path : getSourcePaths(context)) {
-            DexFile dexfile;
-            if (path.endsWith(EXTRACTED_SUFFIX)) {
-                //NOT use new DexFile(path), because it will throw "permission error in /data/dalvik-cache"
-                dexfile = DexFile.loadDex(path, path + ".tmp", 0);
-            } else {
-                dexfile = new DexFile(path);
-            }
-            Enumeration<String> dexEntries = dexfile.entries();
-            while (dexEntries.hasMoreElements()) {
-                String className = dexEntries.nextElement();
-                if (className.contains(packageName)) {
-                    classNames.add(className);
+            DexFile dexfile = null;
+
+            try {
+                if (path.endsWith(EXTRACTED_SUFFIX)) {
+                    //NOT use new DexFile(path), because it will throw "permission error in /data/dalvik-cache"
+                    dexfile = DexFile.loadDex(path, path + ".tmp", 0);
+                } else {
+                    dexfile = new DexFile(path);
+                }
+                Enumeration<String> dexEntries = dexfile.entries();
+                while (dexEntries.hasMoreElements()) {
+                    String className = dexEntries.nextElement();
+                    if (className.contains(packageName)) {
+                        classNames.add(className);
+                    }
+                }
+            } catch (Throwable ignore) {
+                Log.e("ARouter", "Scan map file in dex files made error.", ignore);
+            } finally {
+                if (null != dexfile) {
+                    try {
+                        dexfile.close();
+                    } catch (Throwable ignore) {
+                    }
                 }
             }
         }
