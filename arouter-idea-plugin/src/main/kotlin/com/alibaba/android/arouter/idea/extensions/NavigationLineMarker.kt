cd89863628d43f62f3a21e8bfd414bf2e18b119d
正纬 <zhilong.lzl@alibaba-inc.com>
2018-12-13 14:21:34 +0800

    Add idea plugin module.

---
+++ arouter-idea-plugin/src/main/kotlin/com/alibaba/android/arouter/idea/extensions/NavigationLineMarker.kt
@@ -0,0 +1,11 @@
+package com.alibaba.android.arouter.idea.extensions
+
+/**
+ * Mark navigation target.
+ *
+ * @author zhilong <a href="mailto:zhilong.liu@aliyun.com">Contact me.</a>
+ * @version 1.0
+ * @since 2018/12/13 12:30 PM
+ */
+class NavigationLineMarker {
+}
