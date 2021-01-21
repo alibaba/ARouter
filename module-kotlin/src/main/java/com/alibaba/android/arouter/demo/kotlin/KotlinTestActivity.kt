92215dd80a9d46e314440bff84e774643140115a
Goooler <wangzongler@gmail.com>
2021-01-21 18:16:53 +0800

    Use viewBinding take place kotlin-android-extensions

--- module-kotlin/src/main/java/com/alibaba/android/arouter/demo/kotlin/KotlinTestActivity.kt
+++ module-kotlin/src/main/java/com/alibaba/android/arouter/demo/kotlin/KotlinTestActivity.kt
@@ -2,25 +2,31 @@ package com.alibaba.android.arouter.demo.kotlin
 
 import android.app.Activity
 import android.os.Bundle
+import com.alibaba.android.arouter.demo.kotlin.databinding.ActivityKotlinTestBinding
 import com.alibaba.android.arouter.facade.annotation.Autowired
 import com.alibaba.android.arouter.facade.annotation.Route
 import com.alibaba.android.arouter.launcher.ARouter
-import kotlinx.android.synthetic.main.activity_kotlin_test.*
 
 @Route(path = "/kotlin/test")
 class KotlinTestActivity : Activity() {
 
     @Autowired
-    @JvmField var name: String? = null
+    @JvmField
+    var name: String? = null
+
     @Autowired
-    @JvmField var age: Int? = 0
+    @JvmField
+    var age: Int? = 0
+
+    private lateinit var binding: ActivityKotlinTestBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         ARouter.getInstance().inject(this)  // Start auto inject.
 
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_kotlin_test)
+        binding = ActivityKotlinTestBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
-        content.text = "name = $name, age = $age"
+        binding.content.text = "name = $name, age = $age"
     }
 }
